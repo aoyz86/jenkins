e646d0f6955689c9217b6a76bfc8a12d6940e0ad
root <root@jenkins.vm.cressida.homeunix.net>
2013-02-23 13:31:54 +0000

    first java app

--- java/hello.java
+++ java/hello.java
@@ -1,5 +1,5 @@
 class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.println("Hello Steve!"); // Display the string.
+        System.out.println("Hello Sharon!"); // Display the string.
     }
 }
